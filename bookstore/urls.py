8349034574db0c2ff18ad19af40bea228b524738
Javlonbek <Torajonov3112@gmail.com>
2021-09-12 14:29:40 +0300

    asfsf
    sffasasf

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from bookshop.views import customHandler404 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cart/', include('cart.urls')),
