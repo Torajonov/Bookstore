65185d6f0fd6d2346adc9a2f88d71f344439b766
Javlonbek <85381615+Torajonov@users.noreply.github.com>
2022-11-21 16:59:10 +0500

    askjhjdsgkljsd
    jfslkdjfdskl

--- bookshop/admin.py
+++ bookshop/admin.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from .models import Category, Product, Review, Contact, Post
 admin.site.register(Contact)
 admin.site.register(Post)
-@admin.register(Category)
+@admin.register(Kategoriya)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name',)}
 
-@admin.register(Product)
+@admin.register(Mahsulotlar)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug', 'price','available', 'created', 'updated']
     list_filter = ['available', 'created', 'updated']
--- coupons/admin.py
+++ coupons/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Coupon
 
 
-@admin.register(Coupon)
+@admin.register(Kupon)
 class CouponAdmin(admin.ModelAdmin):
     list_display = ['code', 'valid_from', 'valid_to',
                     'discount', 'active']
