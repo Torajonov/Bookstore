3dd475031744f9ace87dbc82a5daaa8a2b4f9ff3
Javlonbek <85381615+Torajonov@users.noreply.github.com>
2023-01-28 12:13:28 +0500

    update
    i'm also updating this project bookstrore

--- coupons/admin.py
+++ coupons/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Coupon
 
 
-@admin.register(Kupon)
+@admin.register(Coupon)
 class CouponAdmin(admin.ModelAdmin):
     list_display = ['code', 'valid_from', 'valid_to',
                     'discount', 'active']
