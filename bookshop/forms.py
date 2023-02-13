c0c350acca3d33992b01da1703a54f72d67eb767
Torajonov <torajonov3112@gmail.com>
2023-02-13 01:43:34 -0500

    update

--- bookshop/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class CommentForm(forms.Form):
-    name = forms.CharField(label='Name', max_length=100)
-    email = forms.EmailField(label='Email')
-    message = forms.CharField(label='Message', widget=forms.Textarea)
