cfcb4ef81b11fd13f188250539922277b245ec3a
jdu210582 <jdu210582J@jdu.uz>
2023-02-14 03:40:39 -0500

    update

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -2,7 +2,7 @@ from environs import Env
 
 import os
 import dj_database_url
-import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -138,7 +138,7 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 
 
