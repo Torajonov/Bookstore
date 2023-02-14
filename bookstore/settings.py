4db16d9aae6b31b7fd1eb224783fcfd1854c8bd8
jdu210582 <jdu210582J@jdu.uz>
2023-02-14 07:07:41 -0500

    update

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = '$(@kyv*7hkrfmo1p5ea&(ix05_xx7p(paz6bxrgqo-se#q9vt('
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['kia.herokuapp.com','kia.herokuapp.com/admin']
 
 
 # Application definition
